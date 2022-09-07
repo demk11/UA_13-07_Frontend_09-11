001e740d3e979f37ceaa4714359db76d73f723be
Alex Demko <=>
2022-09-07 09:19:01 +0200

    Changed

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
     let el = document.querySelector("#text");
-    el.innerHTML = "<b>Hello World 2</b>";
-    // console.log(el.innerHTML);
-    // console.log(el.textContent);
+
+    console.log(el.outerHTML);
 });
