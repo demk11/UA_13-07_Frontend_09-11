c345edf502ccd98184e3a891006632ae9aaf5dfa
Alex Demko <=>
2022-09-07 09:18:15 +0200

    Changed

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
-    let el = document.querySelectorAll(".text[data-asdf='hello1']");
-
-    console.log(el);
+    let el = document.querySelector("#text");
+    el.innerHTML = "<b>Hello World 2</b>";
+    // console.log(el.innerHTML);
+    // console.log(el.textContent);
 });
