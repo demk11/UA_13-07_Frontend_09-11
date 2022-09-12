5675c5a74903b64c8d13654e7e31292c5f41d3ba
Alex Demko <=>
2022-09-12 08:10:26 +0200

    changed

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-    let el = document.getElementById("checkbox");
-   
-    el.style.backgroundRepeat = "no-repeat";
+    
+    function
+
 });
