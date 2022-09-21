5aa628fa95ad7ad0c06c1f1d85170665f6bc4eb5
Alex Demko <=>
2022-09-21 09:25:02 +0200

    Changed

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-    document.querySelector("#div1").addEventListener("click", function(){
-        alert('div #1!');
-    }, true);
-
-    document.querySelector("#div2").addEventListener("click", function(){
-        alert('div #2!');
-    }, true);
-
-    document.querySelector("#div3").addEventListener("click", function(){
-        alert('div #3!');
-    }, true);
-
-    document.querySelector("button").addEventListener("click", function(){
-        alert('button!');
-    }, true);
+    document.querySelector("form").addEventListener("submit", function(e){
+        e.preventDefault();
+    });
 
 });
